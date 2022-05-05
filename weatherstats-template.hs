4094a2fd365202568fd30082aaff2c1f2744406a
samuelmmorse <samuel.morse@maine.edu>
2022-05-05 17:49:22 -0400

    Update weatherstats-template.hs

--- weatherstats-template.hs
+++ weatherstats-template.hs
@@ -37,7 +37,7 @@ data Observation =
    deriving (Eq, Show)
 
 temp (Observation _ _ t _ _) = t
-hour (Observation _ h _ _ _) = h 
+hour (Observation _ h _ _ _) = h
 
 instance FromNamedRecord Observation where    
    parseNamedRecord m = 
@@ -72,58 +72,79 @@ readData filename = do
 --------------------------------------------
 -- DO NOT CHANGE ANYTHING ABOVE THIS LINE --
 --------------------------------------------
--- Name: PUT YOUR NAME HERE
--- Partner: ...
+-- Name: Samuel Morse
+-- Partner: Matthew Brown
 --------------------------------------------
 
 
 -- Question 1a
-average :: ...
-average ...
+
+myaverage :: [Float] -> Float
+myaverage a = foldl1 (+) a / (mylength a)
 
 -- Question 1b
-maxDiff :: ...
-maxDiff ...
+maxDiff :: [Float] -> Float
+maxDiff a = (maxval a) - (minval a)
 
 -- Question 1c
-daySummary :: ...
-daySummary day = ...
-
+daySummary day = do
+  obsData <- readData "2069-2021.csv"
+  let averagetemp = myaverage (map temp (take 24 (drop (day*24 - 24) obsData)))
+  let maxtempdiff = maxDiff (map temp (take 24 (drop (day*24 - 24) obsData)))
+  let averagewind = myaverage (map wind (take 24 (drop (day*24 - 24) obsData)))
+  let maxwindspeeddiff = maxDiff (map wind (take 24 (drop (day*24 - 24) obsData)))
+  let result = (day, averagetemp, maxtempdiff, averagewind, maxwindspeeddiff)
+  print result
 --- any other functions you need for 1a-1c go here
 
+wind(Observation _ _ _ _ w) = w
+
+mylength :: [Float] -> Float
+mylength [] = 0
+mylength l = 1 + (mylength (tail l))
+
+maxval :: [Float] -> Float
+maxval [] = 0
+maxval a = max (head a) (maxval(tail a))
+
+minval :: [Float] -> Float
+minval [] = 1/0
+minval a = min (head a) (minval(tail a))
+  
+
 -- Question 2a
-chunkby :: ...
-chunkby ...
+-- chunkby :: ...
+-- chunkby ...
 
--- Question 2b
-chunkByDays :: ...
-chunkByDays ...
-
-
--- 3a: add type declaration here 
--- 3a: add an explanation
-dailyTemperatureStat :: ...
-dailyTemperatureStat f day obsData = f (map temp dayList)  
-  where 
-    h = 24*(day-1)
-    dayList = (take 24 (drop h obsData))
+-- -- Question 2b
+-- chunkByDays :: ...
+-- chunkByDays ...
 
 
+-- -- 3a: add type declaration here 
+-- -- 3a: add an explanation
+-- dailyTemperatureStat :: ...
+-- dailyTemperatureStat f day obsData = f (map temp dayList)  
+--   where 
+--     h = 24*(day-1)
+--     dayList = (take 24 (drop h obsData))
 
--- Example function for 3b: Computes the minimum temperature for Jan 3 based on the 24 hourly measurements
-jan3Minimum filename = do 
-  obsData <- readData filename
-  let result = minimum (map temp (take 24 (drop 48 obsData)))
-  putStr "minimum temperature on January 3 = "
-  print (result)
 
--- Question 3b
-allMinimumTemp filename = do 
-  obsData <- readData filename
-  let result = ...
-  putStr "Minimum temperature for each day of the year:"
-  print (result)
 
--- Question 3c
-highDifferentialDays filename = ...
+-- -- Example function for 3b: Computes the minimum temperature for Jan 3 based on the 24 hourly measurements
+-- jan3Minimum filename = do 
+--   obsData <- readData filename
+--   let result = minimum (map temp (take 24 (drop 48 obsData)))
+--   putStr "minimum temperature on January 3 = "
+--   print (result)
+
+-- -- Question 3b
+-- allMinimumTemp filename = do 
+--   obsData <- readData filename
+--   let result = ...
+--   putStr "Minimum temperature for each day of the year:"
+--   print (result)
+
+-- -- Question 3c
+-- highDifferentialDays filename = ...
   
